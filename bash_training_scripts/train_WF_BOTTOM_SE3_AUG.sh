02c2381e0a9d56233b8b30a57972572ebd0c7177
Karan Mudaliar <mudaliar.k@northeastern.edu>
2025-03-18 21:09:47 -0400

    fix

--- bash_training_scripts/train_WF_BOTTOM_SE3_AUG.sh
+++ bash_training_scripts/train_WF_BOTTOM_SE3_AUG.sh
@@ -34,4 +34,4 @@ echo "Content of data directory:"
 ls -l data/
 
 # Run with debug output
-python -u comformer/scripts/train_D2R2_WF_BOTTOM_SE3_AUG.py
+python -u comformer/scripts/train_WF_bottom_augmented_SE3.py
